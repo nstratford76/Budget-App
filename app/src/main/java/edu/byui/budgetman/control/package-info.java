015d663baed4eece4baf414d96654f09063a10ed
Dan Ortega <ortega.dan2010@gmail.com>
2020-03-07 21:16:11 -0500

    Updating packageinfo for control

--- app/src/main/java/edu/byui/budgetman/control/package-info.java
+++ app/src/main/java/edu/byui/budgetman/control/package-info.java
@@ -1,4 +1,5 @@
 /**
- * Here we should have our control classes which provide code functionality
+ * Here we should have our control classes which provide core functionality
+ * and provide a connection between view and model classes
  * */
 package edu.byui.budgetman.control;
