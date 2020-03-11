86b1e1832faa31297d9e7c73f05ac0c8eacff809
Nathan Stratford <nstratford76@gmail.com>
2020-03-11 17:41:59 -0600

    I added the add category activity, with some code for converting textView into a string and made a map to keep track of categories with the amount values.

---
+++ app/src/main/java/edu/byui/budgetman/CategoryView.java
@@ -0,0 +1,41 @@
+package edu.byui.budgetman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CategoryView extends AppCompatActivity {
+
+    TextView input;
+    TextView input2;
+    String category;
+    String amountText;
+
+    // This Map will keep track of the categories and pair them up
+
+    HashMap<Float,String> hm= new HashMap<Float,String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_view);
+    }
+
+    public void getCategory() {
+
+        input = (TextView) findViewById(R.id.editText);
+        category = input.getText().toString();
+
+        input2 = (TextView) findViewById(R.id.editText2);
+        amountText = input.getText().toString();
+
+        Float amount = Float.parseFloat(amountText);
+        hm.put(amount, category);
+        input.setText("");
+        input2.setText("");
+    }
+}
