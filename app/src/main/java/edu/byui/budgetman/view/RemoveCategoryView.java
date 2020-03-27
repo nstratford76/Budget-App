67b6e541e622071e2349d6be2e6ef80aa76452d0
smclaug7 <mcl18013@byui.edu>
2020-03-27 17:56:04 -0600

    Added functionality to remove category it is glitching

--- app/src/main/java/edu/byui/budgetman/view/RemoveCategoryView.java
+++ app/src/main/java/edu/byui/budgetman/view/RemoveCategoryView.java
@@ -1,15 +1,52 @@
 package edu.byui.budgetman.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 import edu.byui.budgetman.R;
+import edu.byui.budgetman.control.BudgetControl;
+import edu.byui.budgetman.model.Budget;
+import edu.byui.budgetman.model.Category;
+import edu.byui.budgetman.model.Transaction;
 
 public class RemoveCategoryView extends AppCompatActivity {
+
+    TextView input;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remove_category_view);
     }
+
+    public void removeCategory(View view) {
+
+        // This variables are for the method more than the class
+        // keeping them in here makes it more testable and possibly reusable
+        String category;
+        String amountT;
+
+        input = (TextView) findViewById(R.id.editText6);
+        category = input.getText().toString();
+
+        Budget budget = BudgetControl.getCurrentMonthBudget();
+        Category cat = budget.getCategoryByName(category);
+
+        for (int i = 0; i < budget.getCategories().size(); i++) {
+            if (budget.getCategories().get(i) == cat) {
+                budget.getCategories().remove(i);
+            }
+        }
+
+        BudgetControl.saveCurrentMonthBudget();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
